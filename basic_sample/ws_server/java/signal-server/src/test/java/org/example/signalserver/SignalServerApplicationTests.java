3ea526e5ff37620b592b8b4349e45fb5862d78a3
zhangzan <zhangzan@tal.com>
2024-11-17 13:28:27 +0800

    java WebSocket初始化

---
+++ basic_sample/ws_server/java/signal-server/src/test/java/org/example/signalserver/SignalServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.signalserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignalServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
